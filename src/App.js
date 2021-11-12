041489a9476c481151e9ce0ba46ea7a5f97fdc7f
rahilchughtai <rahilchughtai1@gmail.com>
2021-11-12 17:10:11 +0100

    Setup: Removing Boilerplate, Adding App logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import dhbwLogo from './assets/dhbwCoolersvg.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <header className="App-header">
+        <h1> DHBW Chat App </h1>
+        <img src={dhbwLogo} className="App-logo" alt="logo" />
+      </header>
+
+      {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +26,7 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
     </div>
   );
 }
