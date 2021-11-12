43014402bc70440b02ff31e453912754b927aaa9
rahilchughtai <rahilchughtai1@gmail.com>
2021-11-12 23:10:59 +0100

    Removing test file, minor style changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'firebase/compat/firestore';
 import { initializeApp } from "firebase/app"
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { getFirestore } from '@firebase/firestore';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { firebaseConfig } from './firebase';
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -18,8 +18,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> DHBW Chat App </h1>
         <img src={dhbwLogo} className="App-logo" alt="logo" />
+        <h1> DHBW Chat App </h1>
       </header>
 
       <main>
@@ -34,19 +34,17 @@ function SignIn() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
   }
-
   return (
-    <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-    </>
+    <><button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button></>
   )
 
 }
 
+/*
 function SignOut() {
   return auth.currentUser && (
     <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
-}
+}*/
 
 export default App;
