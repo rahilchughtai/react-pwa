e12220153612681c87822d5bc4b9f4cb4de4a53b
rahilchughtai <rahilchughtai1@gmail.com>
2021-11-16 15:53:31 +0100

    battery

--- src/components/AppBatteryDisplay/AppBatteryDisplay.js
+++ src/components/AppBatteryDisplay/AppBatteryDisplay.js
@@ -4,15 +4,15 @@ import { Battery } from '@pxblue/react-progress-icons';
 
 export const AppBatteryDisplay = () => {
     const batteryState = useBattery();
-    const batteryPercent = (batteryState.level * 100).toFixed(0)
-
+    const percent = (batteryState.level * 100).toFixed(0)
+    const { charging, isSupported } = batteryState
 
     return (
-        <div hidden={!batteryState.isSupported} >
+        <div hidden={!isSupported} >
             <div className="App-battery">
-                <Battery backgroundColor={'white'} percent={batteryPercent} size={30} color={'teal'} charging={batteryState.charging} outlined={false} />
+                <Battery color={'teal'} percent={percent} size={36}  charging={charging} outlined={false} />
             </div>
-            {batteryPercent}%
+            {percent}%
         </div>
 
     )
